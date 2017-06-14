6d7c00be7841612167e96ac7ee66331c41d7b1ef
Evan Havlin <ehavlin@hotmail.com>
2017-06-13 22:59:48 -0400

    Make Main, ArticleBody, ArticleLInk, ArticleTitle and Footer components

---
+++ src/ArticleTitle.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ArticleTitle extends Component {
+    render() {
+        return (
+            <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+        )
+    }
+}
+
+export default ArticleTitle
