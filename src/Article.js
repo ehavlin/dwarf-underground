9ea1c393a5c127854765a040335b10e0d9e33c15
Evan Havlin <ehavlin@hotmail.com>
2017-06-13 23:11:48 -0400

    Move article components into a new Article component. Make ad component and authorInfo component.

---
+++ src/Article.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import ArticleTitle from './ArticleTitle'
+import ArticleBody from './ArticleBody'
+import ArticleLinks from './ArticleLinks'
+import AuthorInfo from './AuthorInfo'
+
+class Article extends Component {
+    render() {
+        return (
+            <div className="large-8 medium-12 columns article">
+                <ArticleTitle />
+                <AuthorInfo />
+                <ArticleBody />
+                <ArticleLinks />
+            </div>
+        )
+    }
+}
+
+export default Article
